2c045d80b71df6e948b7dab7c1583407a90b1da2
Mudit <muditmakkar29@gmail.com>
2023-02-18 01:32:12 +0530

    fifth commit - memoized the process of filtering todos in TodoList.js

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,5 +1,5 @@
 import styles from "./style.module.css";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import Dropdown from "../Dropdown/Dropdown";
 import Task from "../Task/Task";
 export default function TodoList({ todos, dispatch, handleEditClick }) {
@@ -27,49 +27,59 @@ export default function TodoList({ todos, dispatch, handleEditClick }) {
     });
   };
 
+  //filtering todos based on dropdown value
+  let filteredTodos = useMemo(() => {
+    if (value === "All") {
+      return todos;
+    } else if (value === "Completed") {
+      return todos.filter((task) => {
+        return task.completed === true;
+      });
+    } else if (value === "Pending") {
+      return todos.filter((task) => {
+        return task.completed === false;
+      });
+    }
+  }, [value, todos]);
+
   return (
     <>
       {" "}
-      {todos.length === 0 ? (
-        <h2
-          style={{ color: "seagreen", textAlign: "center", marginTop: "50px" }}
-        >
-          No tasks in the list . . . . .{" "}
-        </h2>
-      ) : (
-        <div className={styles.container}>
-          <ul className={styles.todoList}>
-            <li className={styles.links} key="links">
-              <Dropdown value={value} setValue={setValue} /> &nbsp;
-              <button
-                onClick={deleteCompletedTasks}
-                className={styles.deleteButton}
-              >
-                Delete Completed Tasks
-              </button>
-            </li>
-
-            {todos.map((todo) => {
-              if (
-                value === "All" ||
-                (value === "Completed" && todo.completed) ||
-                (value === "Pending" && !todo.completed)
-              ) {
-                return (
-                  <Task
-                    todo={todo}
-                    handleDeleteTodo={handleDeleteTodo}
-                    handleTaskStatusChange={handleTaskStatusChange}
-                    handleEditClick={handleEditClick}
-                  />
-                );
-              } else {
-                return "";
-              }
-            })}
-          </ul>
-        </div>
-      )}
+      <div className={styles.container}>
+        <ul className={styles.todoList}>
+          <li className={styles.links} key="links">
+            <Dropdown value={value} setValue={setValue} /> &nbsp;
+            <button
+              onClick={deleteCompletedTasks}
+              className={styles.deleteButton}
+            >
+              Delete Completed Tasks
+            </button>
+          </li>
+          {filteredTodos.length !== 0 ? (
+            filteredTodos.map((todo) => {
+              return (
+                <Task
+                  todo={todo}
+                  handleDeleteTodo={handleDeleteTodo}
+                  handleTaskStatusChange={handleTaskStatusChange}
+                  handleEditClick={handleEditClick}
+                />
+              );
+            })
+          ) : (
+            <h2
+              style={{
+                color: "seagreen",
+                textAlign: "center",
+                marginTop: "50px",
+              }}
+            >
+              No tasks in the list . . . . .{" "}
+            </h2>
+          )}
+        </ul>
+      </div>
     </>
   );
 }
